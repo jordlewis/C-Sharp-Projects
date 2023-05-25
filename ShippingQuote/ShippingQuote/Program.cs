7055be2e974e0ad566cabcf50b189538f8d4dcad
jordlewis <ohgoodjord@gmail.com>
2023-05-25 17:25:28 -0500

    Revision to branch statements
    Made changes to the branching statements to end the program if the user input for weight, or the total sum of the user input exceeds '50'. If they do not, the program will run and calculate a shipping quote based off the user input.

--- ShippingQuote/ShippingQuote/Program.cs
+++ ShippingQuote/ShippingQuote/Program.cs
@@ -13,38 +13,49 @@ namespace ShippingQuote
             // This converts the user's input to an integer
             int weight = Convert.ToInt32(Console.ReadLine());
 
-            // This prompts the user to enter the package height
-            Console.WriteLine("Please enter the package height:");
-            // This converts the user's input to an integer
-            int height = Convert.ToInt32(Console.ReadLine());
-
-            // This prompts the user to enter the package width
-            Console.WriteLine("Please enter the the package width:");
-            // This converts the user's input to an integer
-            int width = Convert.ToInt32(Console.ReadLine());
-
-            // This prompts the user to enter the package length
-            Console.WriteLine("Please enter the package length");
-            // This converts the user's input to an integer
-            int length = Convert.ToInt32(Console.ReadLine());
-
-            // This creates the user's total price by multiplying the height x length x width, then dividing by weight
-            int price = ((height * width * length) / weight);
-
-            // This creates a string named 'quote' that displays a message and the total price of the shipping quote, formatted for currency
-            string quote = ("Your estimated total for shipping this package is: " + price.ToString("C"));
-
             // This is the if/esle statement to advise the user that their package does not meet requirements, or provides the actual quote tailored to the user's input
-            if (50 < (weight + height + width + length))
+            if (weight > 50)
             {
-                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
             }
-            else            
+            else
             {
-                Console.WriteLine(quote);
+                // This prompts the user to enter the package width
+                Console.WriteLine("Please enter the the package width:");
+                // This converts the user's input to an integer
+                int width = Convert.ToInt32(Console.ReadLine());
+
+                // This prompts the user to enter the package height
+                Console.WriteLine("Please enter the package height:");
+                // This converts the user's input to an integer
+                int height = Convert.ToInt32(Console.ReadLine());
+
+                // This prompts the user to enter the package length
+                Console.WriteLine("Please enter the package length");
+                // This converts the user's input to an integer
+                int length = Convert.ToInt32(Console.ReadLine());
+
+                int dimensions = (length + width + height);
+
+                if (dimensions > 50) {
+
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+
+                else {
+                    // This creates the user's total price by multiplying the height x length x width, then dividing by weight
+                    int price = (((length * width * height) * weight) / 100) ;
+
+                    // This creates a string named 'quote' that displays a message and the total price of the shipping quote, formatted for currency
+                    string quote = ("Your estimated total for shipping this package is: " + price.ToString("C"));
+
+                    Console.WriteLine(quote);
+                }
             }
 
             Console.ReadLine();
+
+
 
         }
     }
