1ba9ec3c224fc713a28f3fc0a7ee3557e42099f5
jordlewis <ohgoodjord@gmail.com>
2023-06-08 21:53:36 -0500

    21 Card Game

---
+++ MainMethodDemo/MainMethodDemo/MathClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethodDemo
+{
+    public class MathClass
+    {
+        public int MathOne(int numbaOne)
+        {
+            int remainder = 10 - numbaOne;
+            return remainder;
+        }
+    }
+}
---
+++ MainMethodDemo/MainMethodDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MainMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This instantiates 'classOne'
+            MathClass classOne = new MathClass();
+
+            int numbaOne = 2;
+            int methodOne = classOne.MathOne(numbaOne);
+            Console.WriteLine("The remainder of MathOne is {0}", methodOne);
+            Console.ReadLine();
+        }
+    }
+}
