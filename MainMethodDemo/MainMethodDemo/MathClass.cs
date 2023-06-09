4c6b190822ecc6e715ba3f7cc99bc7a11513a844
jordlewis <ohgoodjord@gmail.com>
2023-06-08 22:21:48 -0500

    Main Method Demo
    This is a chunk of code to demonstrate overloading a class.

--- MainMethodDemo/MainMethodDemo/MathClass.cs
+++ MainMethodDemo/MainMethodDemo/MathClass.cs
@@ -6,10 +6,30 @@ namespace MainMethodDemo
 {
     public class MathClass
     {
-        public int MathOne(int numbaOne)
+        public int MyMath(int numbaOne)
         {
             int remainder = 10 - numbaOne;
             return remainder;
         }
+
+        public int MyMath(decimal numbaTwo)
+        {
+            int product = Convert.ToInt32(numbaTwo * 88);
+            return product;
+        }
+        public int MyMath(string numbaThree)
+        {
+            // This sets the value of the variable 'sum' to 0
+            int sum = 0;
+            // This takes the string parameter and converts it to an integer--if possible. 
+            if (int.TryParse(numbaThree, out int parse))
+            {
+                // If the string was converted, it will add 21 to it
+                sum = parse + 21;
+            }
+            // This return the value of 'sum', depending on if the string was able to be parsed.
+            return sum;
+        }
     }
+    
 }
--- MainMethodDemo/MainMethodDemo/Program.cs
+++ MainMethodDemo/MainMethodDemo/Program.cs
@@ -9,10 +9,30 @@ namespace MainMethodDemo
             // This instantiates 'classOne'
             MathClass classOne = new MathClass();
 
+            // This assigns a value of 2 to the variable 'numbaOne'
             int numbaOne = 2;
-            int methodOne = classOne.MathOne(numbaOne);
+            // This will pass the numbaOne variable to the classOne method
+            int methodOne = classOne.MyMath(numbaOne);
+            // This will print a statement and returns an integer
             Console.WriteLine("The remainder of MathOne is {0}", methodOne);
+
+            // This assigns a value of 2 to the variable 'numbaTwo'
+            decimal numbaTwo = 9.3m;
+            // This will pass the numbaTwo variable to the classOne method
+            int methodTwo = classOne.MyMath(numbaTwo);
+            // This will pass the numbaTwo variable to the classOne method
+            Console.WriteLine("The product of MathOne is {0}", methodTwo);
+
+            // This assigns a value of 2 to the variable 'numbaThree'
+            string numbaThree = "9";
+            // This will pass the numbaThree variable to the classOne method
+            int methodThree = classOne.MyMath(numbaThree);
+            // This will pass the numbaThree variable to the classOne method
+            Console.WriteLine("The sum of MathOne is {0}", methodThree);
+
             Console.ReadLine();
         }
+
+
     }
 }
