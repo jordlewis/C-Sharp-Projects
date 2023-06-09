76d729d784439c4e4ba64cd838527afecefaf2b4
jordlewis <ohgoodjord@gmail.com>
2023-06-08 20:59:44 -0500

    Calling Methods
    This is a small chunk of code to demonstrate calling methods

---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass testClass = new MyClass();
+            Console.WriteLine("Please ennter a number to pass through 3 different methods:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            int meth1 = testClass.timesTen(userInput);
+            Console.WriteLine("{0} * 10 = {1}", userInput, meth1);
+            int meth2 = testClass.inHalf(userInput);
+            Console.WriteLine("{0} / by 2 = {1}", userInput, meth2);
+            int meth3 = testClass.highNum(userInput);
+            Console.WriteLine("{0} +  3521479 = {1}", userInput, meth3);
+
+
+
+            Console.ReadLine();
+
+        }
+    }        
+}
