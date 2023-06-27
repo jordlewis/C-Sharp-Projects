0853ca35d9f667ccb4bb895823a71a75ed22aba4
jordlewis <ohgoodjord@gmail.com>
2023-06-26 23:58:59 -0500

    Code First Demo
    This is a basic Code-First condole application written in C#.

--- CoreApp/CoreApp/Pages/Time.cshtml.cs
+++ CoreApp/CoreApp/Pages/Time.cshtml.cs
@@ -6,9 +6,12 @@ namespace CoreApp.Pages
 {
     public class TimeModel : PageModel
     {
+        // 
         public DateTime ServTime { get; set; }
+        // This is called when the page is loaded
         public void OnGet()
         {
+            // And set's the variable 'ServTime' to the current server time
             ServTime = DateTime.Now;
         }
 
